512e953983f1a629741fab80e96a20737c1fc739
CyrusRice <rice9@pdx.edu>
2021-07-09 23:47:46 +0000

    t4est

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='password', 
-    version='0.1',
+    version='0.2',
     description='Password Python Package', 
     author='@huy', 
     author_email='', 
